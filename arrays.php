be8333bfcfb9a61ab4c73460f4201e58b20f5d96
LukeLikesCat <90268975+LukeLikesCat@users.noreply.github.com>
2021-09-07 13:06:57 -0400

    Update arrays.php

--- arrays.php
+++ arrays.php
@@ -1,11 +1,15 @@
 <?php 
 
+/*you can add whatever website you want here, the Post request is just for specifying the google search query.*/
 $web_mod = array(
     "https://www.google.com/search?q=".$_POST["search"]."&tbm=vid",
     "https://www.google.com/search?q=".$_POST["search"]."&tbm=nws",
     "https://www.google.com/search?q=".$_POST["search"]."&tbm=shop"
 );
 
+/*searches the html for the nodes with the data I want.
+The keys in the "str_mods" array and this array are labels
+of the data that is being taken.*/
 $xpath_query = array(
     array(
         'title' => '//h3[@class="zBAuLc"]//div[@class="BNeawe vvjwJb AP7Wnd"]',
@@ -27,6 +31,8 @@ $xpath_query = array(
    
 );
 
+/*uses regular expresions to take off the extra stuff 
+from the xpath string that I dont want.*/
 $str_mods = array(
     array(
         'title' => '~~',
@@ -49,6 +55,8 @@ $str_mods = array(
 
 );
 
+/*takes the number of pages that I want to take from and 
+turns it into the url format*/
 $pages = array("");
 
 $pp = $_POST["page_num"] - 1;
